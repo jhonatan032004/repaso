ef9c241bf5cff71b79e7a3c3072837f46946da24
ddnarvaez <dandnarvaez@misena.edu.co>
2023-03-24 17:37:28 -0500

    controlador

--- app/app.js
+++ app/app.js
@@ -2,16 +2,11 @@ import express from "express";
 import message from "./config/message.js";
 import enviroment from "./config/enviroments";
 import allRoutes from "./routes/user.routes";
-
-
 const app = express();
+
 app.set("PORT",process.env.PORT || 3000);
 
 app.use(express.json());
-
 app.use("/api",allRoutes);
-
-
-
 
 export default app;
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -11,7 +11,8 @@ route.get("/", (req, res)=>{
 route.get("/user", controller.findAllUser);
 route.get("/user/:id", controller.findUser);
 route.post("/user", controller.insertUser);
-
+route.put("/user", controller.updateUser);
+route.delete("/user/:id", controller.deleteUser);
 
 
 export default route;
