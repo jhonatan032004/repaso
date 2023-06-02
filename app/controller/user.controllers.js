ab549a9439ecdc371c4a33e1005eade6dadf08df
ddnarvaez <dandnarvaez@misena.edu.co>
2023-06-02 14:39:04 -0500

    solucion del name

--- app/controller/user.controllers.js
+++ app/controller/user.controllers.js
@@ -43,7 +43,7 @@ export const deleteUser = async (req, res)=>{
 }
 export const updateUser = async (req, res)=>{
     const id = req.body.id;
- /////   const name = req.body.name;
+    const name = req.body.name;
     try {
         console.log("hola")
         const result = await pool.query(`CALL spUpdateUser(${id},'${name}');`);
